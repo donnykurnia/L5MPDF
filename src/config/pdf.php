93659373b50965e855ad13d44e676af5c641a3a2
Donny Kurnia <donnykurnia@gmail.com>
2016-04-04 11:21:12 +0700

    add default config

---
+++ src/config/pdf.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'mode'                  => '',
+    'format'                => 'A4',
+    'defaultFontSize'       => '',
+    'defaultFont'           => '',
+    'marginLeft'            => 10,
+    'marginRight'           => 10,
+    'marginTop'             => 10,
+    'marginBottom'          => 10,
+    'marginHeader'          => 10,
+    'marginFooter'          => 5,
+    'orientation'           => 'P',
+
+    'title'                 => 'Laravel mpdf wrapper',
+    'author'                => 'mPDF Library',
+    'watermark'             => '',
+    'showWatermark'         => false,
+    'watermarkFont'         => 'DejaVuSansCondensed',
+    'displayMode'           => 'fullpage',
+    'watermarkTextAlpha'    => 0.1,
+];
